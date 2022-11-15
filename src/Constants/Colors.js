9e61eb7476524fd1e22820955e2dc5e78002be6b
LuckyFox <noah.grebet@orange.fr>
2022-11-15 13:36:54 +0100

    :sparkles: 18 - Add Header

---
+++ src/Constants/Colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+	white: '#FFFFFF',
+	black: '#000000',
+	ghost: '#F4F6F6',
+	dark_blue: '#2E4053',
+	blue: '#2E86C1',
+	gray: '#868686',
+	light_gray: '#EFEFEF',
+	red: '#E74C3C',
+}
