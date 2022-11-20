42030e14171e0db9e8d671cfb28e3d63cb0f58b4
LuckyFox <noah.grebet@orange.fr>
2022-11-20 16:23:42 +0100

    :art: 21 - Move Presentation section in molecules

---
+++ src/Molecules/LinksSection.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import {COLORS} from "../Constants/Colors.js";
+import {BORDER} from "../Constants/Border.js";
+import {SHADOW} from "../Constants/Shadow.js";
+
+export default function LinksSection(){
+	return (
+		<Container>
+
+		</Container>
+	)
+}
+
+const Container = styled.section`
+	background-color: ${COLORS.white};
+	border-radius: ${BORDER.radius};
+  	box-shadow: ${SHADOW.small};
+`;
