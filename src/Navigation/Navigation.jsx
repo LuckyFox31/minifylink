89612acc8ca38231f3276f38e9c497fead291c58
LuckyFox <noah.grebet@orange.fr>
2022-11-26 17:41:45 +0100

    :art: 16 - Add redirection in router

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -3,8 +3,13 @@ import Template from "../Layouts/Template.jsx";
 
 // Pages
 import Homepage from "../Pages/Homepage";
+import Redirection from "../Pages/Redirection";
 
 export const Navigation = [
+	{
+		path: "/:link",
+		element: <Redirection />
+	},
 	{
 		path: "/",
 		element: <Template />,
---
+++ src/Pages/Redirection.jsx
@@ -0,0 +1,7 @@
+export default function Redirection() {
+	return (
+		<>
+			<h1>Redirection</h1>
+		</>
+	)
+}
